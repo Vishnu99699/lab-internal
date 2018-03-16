42ee1e8e715836d916d0c1da48639b740fc38d29
Vishnu99699 <37442366+Vishnu99699@users.noreply.github.com>
2018-03-16 17:18:59 +0530

    Add files via upload

---
+++ wt lab/filename.php
@@ -0,0 +1,8 @@
+<?php>
+$servername="localhost";
+$username="root";
+$passward="";
+$db="vishnu";
+$conn = mysqli_connect{$servername,$usernsme,$passward}or die{connection failed: ".mysql_connect()};
+mysqli_select_db($conn,$db);
+?>
